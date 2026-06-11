60a6f67114e0dd5742c5cf1fab451505401dac53
MdAnasChhipa <anaschhipa715@gmail.com>
2026-06-12 02:06:06 +0530

    Initial project setup and backend foundation

---
+++ backend/src/test/java/com/campusplacement/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusplacement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
